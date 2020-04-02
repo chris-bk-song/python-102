866286938b017e0c0e12919f2471b5a13df75d90
Chris Song <chris.bk.song@gmail.com>
2020-04-02 09:21:13 -0400

    fixed script for bonus question #1

--- bonus_question.py
+++ bonus_question.py
@@ -1,4 +1,4 @@
-three_in_one = input('what is the name of your first pet? ')
-three_in_one = input('what is the name of the street you grew up on? ')
-three_in_one = input('Give me an adjective word! ')
+three_in_one = input('what is the name of your first pet? ') + ''
+three_in_one = three_in_one + input('what is the name of the street you grew up on? ') + ''
+three_in_one = three_in_one + input('Give me an adjective word! ')
 print(f'Hello, the {three_in_one}!')
